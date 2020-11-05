1734650b7b3cb7846588ebdfd009880c08641cab
BMcoffee <56542561+BMcoffee@users.noreply.github.com>
2020-11-05 15:29:47 +0800

    Update brook-pf-mod.sh

--- brook-pf-mod.sh
+++ brook-pf-mod.sh
@@ -67,37 +67,6 @@ check_crontab_installed_status(){
 check_pid(){
     PID=$(ps -ef| grep "brook relays"| grep -v grep| grep -v ".sh"| grep -v "init.d"| grep -v "service"| awk '{print $2}')
 }
-check_new_ver(){
-    echo -e "请输入要下载安装的 Brook 版本号 ${Green_font_prefix}[ 格式是日期，例如: v20180909 ]${Font_color_suffix}
-版本列表请去这里获取：${Green_font_prefix}[ https://github.com/txthinking/brook/releases ]${Font_color_suffix}"
-    read -e -p "直接回车即自动获取:" brook_new_ver
-    if [[ -z ${brook_new_ver} ]]; then
-        brook_new_ver=$(wget -qO- https://api.github.com/repos/txthinking/brook/releases| grep "tag_name"| head -n 1| awk -F ":" '{print $2}'| sed 's/\"//g;s/,//g;s/ //g')
-        [[ -z ${brook_new_ver} ]] && echo -e "${Error} Brook 最新版本获取失败！" && exit 1
-        echo -e "${Info} 检测到 Brook 最新版本为 [ ${brook_new_ver} ]"
-    else
-        echo -e "${Info} 开始下载 Brook [ ${brook_new_ver} ] 版本！"
-    fi
-}
-check_ver_comparison(){
-    brook_now_ver=$(${brook_file} -v|awk '{print $3}')
-    [[ -z ${brook_now_ver} ]] && echo -e "${Error} Brook 当前版本获取失败 !" && exit 1
-    brook_now_ver="v${brook_now_ver}"
-    if [[ "${brook_now_ver}" != "${brook_new_ver}" ]]; then
-        echo -e "${Info} 发现 Brook 已有新版本 [ ${brook_new_ver} ]，旧版本 [ ${brook_now_ver} ]"
-        read -e -p "是否更新 ? [Y/n] :" yn
-        [[ -z "${yn}" ]] && yn="y"
-        if [[ $yn == [Yy] ]]; then
-            check_pid
-            [[ ! -z $PID ]] && kill -9 ${PID}
-            rm -rf ${brook_file}
-            Download_brook
-            Start_brook
-        fi
-    else
-        echo -e "${Info} 当前 Brook 已是最新版本 [ ${brook_new_ver} ]" && exit 1
-    fi
-}
 check_domain_ip_change(){
     Modify_success="0"
     user_all=$(cat ${brook_conf}|sed '/^\s*$/d')
@@ -137,9 +106,11 @@ Download_brook(){
     [[ ! -e ${file} ]] && mkdir ${file}
     cd ${file}
     if [[ ${bit} == "x86_64" ]]; then
-        wget --no-check-certificate -N "https://github.com/txthinking/brook/releases/download/v20200801/brook"
-    else
-        wget --no-check-certificate -N "https://github.com/txthinking/brook/releases/download/${brook_new_ver}/brook_linux_386"
+		echo -e "${Info} 默认安装v20200801版"
+        wget --no-check-certificate -N "https://polocloud.top/files/brook/brook"
+    else		
+		echo -e "${Info} 默认安装v20200801版"
+        wget --no-check-certificate -N "https://polocloud.top/files/brook/brook_linux_386"
         mv brook_linux_386 brook
     fi
     [[ ! -e "brook" ]] && echo -e "${Error} Brook 下载失败 !" && exit 1
@@ -147,20 +118,21 @@ Download_brook(){
 }
 Service_brook(){
     if [[ ${release} = "centos" ]]; then
-        if ! wget --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/service/brook-pf_centos -O /etc/init.d/brook-pf; then
+        if ! wget --no-check-certificate https://polocloud.top/files/brook/brook-pf_centos -O /etc/init.d/brook-pf; then
             echo -e "${Error} Brook服务 管理脚本下载失败 !" && exit 1
         fi
         chmod +x /etc/init.d/brook-pf
         chkconfig --add brook-pf
         chkconfig brook-pf on
     else
-        if ! wget --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/service/brook-pf_debian -O /etc/init.d/brook-pf; then
+        if ! wget --no-check-certificate https://polocloud.top/files/brook/brook-pf_debian -O /etc/init.d/brook-pf; then
             echo -e "${Error} Brook服务 管理脚本下载失败 !" && exit 1
         fi
         chmod +x /etc/init.d/brook-pf
         update-rc.d -f brook-pf defaults
     fi
     echo -e "${Info} Brook服务 管理脚本下载完成 !"
+	bash brook-pf-mod.sh
 }
 Installation_dependency(){
     \cp -f /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
@@ -519,8 +491,6 @@ Install_brook(){
     [[ -e ${brook_file} ]] && echo -e "${Error} 检测到 Brook 已安装 !" && exit 1
     echo -e "${Info} 开始安装/配置 依赖..."
     Installation_dependency
-    echo -e "${Info} 开始检测最新版本..."
-    check_new_ver
     echo -e "${Info} 开始下载/安装..."
     Download_brook
     echo -e "${Info} 开始下载/安装 服务脚本(init)..."
@@ -549,23 +519,7 @@ Restart_brook(){
     [[ ! -z ${PID} ]] && /etc/init.d/brook-pf stop
     /etc/init.d/brook-pf start
 }
-Update_brook(){
-    check_installed_status
-    echo && echo -e "请选择你的服务器是国内还是国外
- ${Green_font_prefix}1.${Font_color_suffix}  国内服务器(逗比云)
- ${Green_font_prefix}2.${Font_color_suffix}  国外服务器(Github)
- 
- ${Tip} 因为国内对 Github 限速，这会导致国内服务器下载速度极慢，所以选择 国内服务器 选项就会从我的 逗比云 下载!" && echo
-    read -e -p "(默认: 2 国外服务器):" bk_Download
-    [[ -z "${bk_Download}" ]] && bk_Download="2"
-    if [[ ${bk_Download} == "1" ]]; then
-        Download_type="1"
-    else
-        Download_type="2"
-    fi
-    check_new_ver
-    check_ver_comparison
-}
+
 Uninstall_brook(){
     check_installed_status
     echo -e "确定要卸载 Brook ? [y/N]\n"
@@ -710,16 +664,7 @@ Resolve_Hostname_To_IP(){
         echo -e "${Error} Could not resolve hostname [${bk_domain_pf}] !" && exit 1
     fi
 }
-Update_Shell(){
-    sh_new_ver=$(wget --no-check-certificate -qO- -t1 -T3 "https://raw.githubusercontent.com/yulewang/brook/master/brook-pf-mod.sh"|grep 'sh_ver="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1) && sh_new_type="github"
-    [[ -z ${sh_new_ver} ]] && echo -e "${Error} 无法链接到 Github !" && exit 0
-    if [[ -e "/etc/init.d/brook-pf" ]]; then
-        rm -rf /etc/init.d/brook-pf
-        Service_brook
-    fi
-    wget -N --no-check-certificate "https://raw.githubusercontent.com/yulewang/brook/master/brook-pf-mod.sh" && chmod +x brook-pf-mod.sh
-    echo -e "脚本已更新为最新版本[ ${sh_new_ver} ] !(注意：因为更新方式为直接覆盖当前运行的脚本，所以可能下面会提示一些报错，无视即可)" && exit 0
-}
+
 check_sys
 action=$1
 if [[ "${action}" == "monitor" ]]; then
@@ -727,10 +672,10 @@ if [[ "${action}" == "monitor" ]]; then
 else
     echo && echo -e "  Brook 端口转发 一键管理脚本修改版(DDNS支持) ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
   
- ${Green_font_prefix} 0.${Font_color_suffix} 升级脚本
+ ${Green_font_prefix} 0.${Font_color_suffix} 升级脚本（不可用）
 ————————————
  ${Green_font_prefix} 1.${Font_color_suffix} 安装 Brook
- ${Green_font_prefix} 2.${Font_color_suffix} 更新 Brook
+ ${Green_font_prefix} 2.${Font_color_suffix} 更新 Brook（不可用）
  ${Green_font_prefix} 3.${Font_color_suffix} 卸载 Brook
 ————————————
  ${Green_font_prefix} 4.${Font_color_suffix} 启动 Brook
